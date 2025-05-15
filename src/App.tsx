65e711c73986c9605400c8428d6442b6279bdca2
enurv <var.eda.nur@gmail.com>
2025-05-15 21:02:51 +0300

    feat: ✨ create HouseList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Banner from "./components/Banner";
 import "./App.css";
+import HouseList from "./components/HouseList";
 
 function App() {
   return (
     <>
       <Banner>"Providing houses all over the world"</Banner>
+      <HouseList />
     </>
   );
 }
