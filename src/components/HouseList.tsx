1d39c6bddf95bbecd26b7fbbe94ddcba81730460
enurv <var.eda.nur@gmail.com>
2025-05-16 15:41:15 +0300

    feat: ✨ add house button

--- src/components/HouseList.tsx
+++ src/components/HouseList.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import HouseRow from "./HouseRow";
 
-const houses = [
+const houseArray = [
   {
     id: 1,
     address: "12 Valley of Kings, Geneva",
@@ -16,6 +17,28 @@ const houses = [
 ];
 
 const HouseList = () => {
+  //houses state is readonly use setHouses to change the state
+  const [houses, setHouses] = useState(houseArray);
+
+  //const [counter, setCounter] = useState(0);
+  //! setCounter can take a function parameter that gets the current state
+  //! if there is multiple call to setter react can batch those calls for efficiency only updating the state when batch completes
+  //! this approach ensures that current is set by the previous call hence this approach is reccomended
+  //setCounter((current) => current + 1);
+
+  const addHouse = () => {
+    //set functions have to change the reference so it needs to create new array
+    setHouses([
+      ...houses,
+      {
+        id: 6,
+        address: "32 Valley Way, New york",
+        country: "USA",
+        price: 1000000,
+      },
+    ]);
+  };
+
   return (
     <>
       <div className="row mb-2">
@@ -35,12 +58,19 @@ const HouseList = () => {
           {houses.map((h, index) => (
             // key property is needed whenever an array of Reacts elements is created on the jsx/tsx
             // if there is no unique id value you can use the map index as a last resort but this can cause problems when the item orders have changed
-            <HouseRow address={h.address} country={h.country} price={h.price} key={h.id} />
+            <HouseRow
+              address={h.address}
+              country={h.country}
+              price={h.price}
+              key={h.id}
+            />
             // <HouseRow {...h} key={h.id} />  object destructing alternative but could cause performance issues
-
           ))}
         </tbody>
       </table>
+      <button className="btn btn-primary" onClick={addHouse}>
+        Add
+      </button>
     </>
   );
 };
