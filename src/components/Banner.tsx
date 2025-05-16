466705f376686ccd3dad6266cbe42eb607205d4d
enurv <var.eda.nur@gmail.com>
2025-05-17 00:09:19 +0300

    refactor: ♻️ use style import

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
 import logo from "../assets/GloboLogo.png";
-import { logo as logoClass } from "./Banner.module.css";
-
+import styles from "./Banner.module.css"
 //! important to remember to not put this into component function since it gets called everytime it is re-rendered
 //! since this is a private member of the module this css is isolated
 //! but this approach is not encouraged
@@ -16,7 +15,7 @@ const Banner = ({ children }: {children: ReactNode}) => {
   return (
     <header className="row mb-4">
       <div className="col-5">
-        <img src={logo} className={logoClass} alt="logo" />
+        <img src={logo} className={styles.logo} alt="logo" />
       </div>
       <div className="col-7 mt-5" style={subtitleStyle}>
         {children}
