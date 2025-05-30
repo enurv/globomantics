f0ffdd88adcde00401134366883227bc194b76df
enurv <var.eda.nur@gmail.com>
2025-05-31 00:57:09 +0300

    fix: 🐛 remove custom routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { useCallback, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Banner from "./components/Banner";
-import ErrorBoundary from "./components/ErrorBoundary";
-import navValues from "./navigation/navValues";
-import navigationContext from "./navigation/navigationContext";
-import ComponentPicker from "./components/ComponentPicker";
-import type { houseModel } from "./components/HouseList";
-import { BrowserRouter, Route, Routes } from "react-router";
 import HouseList from "./components/HouseList";
-import House from "./components/house";
+import House from "./components/House";
 
 function App() {
-  const navigate = useCallback(
-    (navTo: string, param: houseModel | null) =>
-      setNav({ current: navTo, param, navigate }),
-    []
-  );
-  const [nav, setNav] = useState<{
-    current: string;
-    param: houseModel | null;
-    navigate: (navTo: string, param: houseModel | null) => void;
-  }>({ current: navValues.home, param: null, navigate });
-
   return (
     <BrowserRouter>
       <Banner>
@@ -32,11 +15,6 @@ function App() {
         <Route index element={<HouseList />} />
         <Route path="house/:id" element={<House />} />
       </Routes>
-      <navigationContext.Provider value={nav}>
-        <ErrorBoundary fallback="Something went wrong">
-          <ComponentPicker currentNavLocation={nav.current} />
-        </ErrorBoundary>
-      </navigationContext.Provider>
     </BrowserRouter>
   );
 }
